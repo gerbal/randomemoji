d094e5c2e2556fac81d0824c117bb9e3ba52b0d5
gerbal <grant.mclendon@unc.edu>
2013-10-14 20:54:16 -0400

    second commit

---
+++ emojilist.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+
+
+soup = BeautifulSoup(open("index.html"))
+emojilist = []
+
+for emoji in soup.find_all('span'):
+	print emoji.string
+	emojilist += [str(emoji.string)]
+print emojilist
+
+output = open("output",'w')
+output.write(str(emojilist))
+output.close
+#print emojilist
